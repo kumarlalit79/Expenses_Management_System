97e4f997766cc2bb681110e2484dc90da092bf97
kumarlalit79 <luckyarya0101@gamil.com>
2024-08-31 15:07:35 +0530

    fixed excel disposal

--- Expenses_Management_System/Controllers/ExcelPackage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Expenses_Management_System.Controllers
-{
-    internal class ExcelPackage : IDisposable
-    {
-        public object Workbook { get; internal set; }
-    }
-}
